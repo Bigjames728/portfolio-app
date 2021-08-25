c65e9aeac910ef83500e89a3ab927d47f59396db
Bigjames728 <bigjames728@gmail.com>
2021-08-24 21:27:43 -0700

    Added routes and imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 // CSS Styles
+import './index.css'
 
 // Components
+import Header from './components/Header';
+import About from './components/About';
+import Articles from './components/Articles';
+import Projects from './components/Projects';
 
 
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="" component={} />
-      </Switch>
+      <div className="App">
+        <Header />
+        <Switch>
+            <Route exact path="/" component={Projects} />
+            <Route exact path="/articles" component={''} />
+            <Route exact path="/about" component={About} />
+        </Switch>
+      </div>
     </Router>
   );
 }
