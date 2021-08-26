abb75a183a3f81798123507c1441c2547a8ead0b
Bigjames728 <bigjames728@gmail.com>
2021-08-25 18:11:10 -0700

    Trying to get ProjectDetails.js working

---
+++ src/components/ProjectDetails.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+
+
+const ProjectDetails = ({ match }) => {
+    const {
+        params: { id },
+    } = match;
+
+    const [projects, setProjects] = useState(null);
+
+    const getData = () => {
+        fetch('data.json'
+        ,{
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        }
+        )
+        .then(response => response.json())
+        .then((projects) => {
+            console.log(projects.data.projects[0]);
+            setProjects(projects.data.projects);
+            
+        })
+    };
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <div>
+            Project Details Page: <strong>{id}</strong>
+        </div>
+    );
+};
+
+export default ProjectDetails;
+
+
+
+
+
+
+
+
+
+
+
+// export default function ProjectDetails() {
+
+//     const [projects, setProjects] = useState([]);
+
+//     const getData = () => {
+//         fetch('data.json'
+//         ,{
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json'
+//             }
+//         }
+//         )
+//         .then(response => response.json())
+//         .then((projects) => {
+//             console.log(projects.data.projects[0]);
+//             setProjects(projects.data.projects);
+            
+//         })
+//     };
+
+//     useEffect(() => {
+//         getData()
+//     },[]) 
+
+    
+
+//     return (
+//         <main>
+//             <div className="flex-container">
+            
+                
+//                 <div className="wrap">
+//                     <h2>Project Details</h2>
+//                         <form>
+//                             <div className="main--flex">
+//                                 <div>
+//                                     <h3 className="course--detail--title">Project</h3>
+//                                     <h4 className="course--name">{projects.project_name}</h4>
+                                    
+                                    
+//                                 </div>
+//                                 <div>
+//                                     <h3 className="course--detail--title">Technologies Used</h3>
+//                                     <ul className="course--detail--list">
+//                                     {projects.technologies}
+//                                     </ul>
+//                                 </div>
+//                             </div>
+//                         </form>
+//                 </div>
+                
+            
+//             </div>
+//         </main>
+//     )
+// }
