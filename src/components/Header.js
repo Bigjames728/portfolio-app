514f1d72c7ad6700f47f638b28ee48353e4c9693
Bigjames728 <bigjames728@gmail.com>
2021-08-31 13:10:10 -0700

    Rewrote the code in Header.js to add active tab functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,65 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../index.css'
 
-export default function Header() {
-    return (
-        <header>
+class Header extends React.Component {
+    
+    state = {
+        links: [
+            { 
+                id: 1, name: 'About', url: '/about', value: '1'
+            },
+            { 
+                 id: 2, name: 'Articles', url: '/articles', value: '2'
+            },
+            { 
+                id: 3, name: 'Projects', url: '/projects', value: '3'
+            }
+        ]
+    };
+
+    render() {
+        const { links } = this.state;
+
+        return (
             <div className="header--flex">
-                <img src='images/logo.PNG' className="header--logo" alt="Logo" />
-            
-                <div className="nav">
-                    <a href="/" className="item">Projects</a>
-                    <a href="/articles" className="item">Articles</a>
-                    <a href="/about" className="item">About</a>
-                </div>
+                <header>
+                    <img src='images/logo.PNG' className="header--logo" alt="Logo" />
+                </header>
+
+                {links.map(link => {
+                    return (
+                        <div className="nav" key={link.id}>  
+                            <NavLink
+                                to={link.url}
+                                className="item"
+                                activeClassName="active"
+                                exact={true}
+                            >{link.name}</NavLink>
+                        </div>
+                    );
+                })}
             </div>
-        </header>
-    );
+        );
+    }
 }
+
+export default Header;
+
+// Old code for original <a> tags used for buttons
+
+// export default function Header() {
+//     return (
+//         <header>
+//             <div className="header--flex">
+//                 <img src='images/logo.PNG' className="header--logo" alt="Logo" />
+            
+//                 <div className="nav">
+//                     <a href="/" className="item">Projects</a>
+//                     <a href="/articles" className="item">Articles</a>
+//                     <a href="/about" className="item">About</a>
+//                 </div>
+//             </div>
+//         </header>
+//     );
+// }
