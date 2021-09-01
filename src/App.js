a2686a7a4f9807231a89b9f9a3001f3b39a173d9
Bigjames728 <bigjames728@gmail.com>
2021-09-01 15:56:18 -0700

    Added Resume.js component to Route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './index.css'
 // Components
 import Header from './components/Header';
 import About from './components/About';
-// import Resume from './components/Resume';
+import Resume from './components/Resume';
 import Projects from './components/Projects';
 import ProjectDetails from './components/ProjectDetails';
 
@@ -23,7 +23,7 @@ function App() {
             <Route exact path="/" render={() => <Redirect to="/projects" />} />
             <Route exact path="/projects" component={Projects} />
             <Route path="/projects/:id" render={({match}) => <ProjectDetails id={match.params.id} />} />
-            {/* <Route exact path="/resume" component={Resume} /> */}
+            <Route exact path="/resume" component={Resume} />
             <Route exact path="/about" component={About} />
         </Switch>
       </div>
