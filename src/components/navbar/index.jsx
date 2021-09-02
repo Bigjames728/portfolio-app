fa4812248bc7f9a07b13f36df05e4f7395ba174f
Bigjames728 <bigjames728@gmail.com>
2021-09-02 15:52:39 -0700

    Component where I put together all the smaller components to make the main NavBar component

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { useMediaQuery } from 'react-responsive';
+import styled from 'styled-components';
+import { Logo } from '../logo';
+import { DeviceSize } from '../responsive';
+import { Login } from './login';
+import { NavLinks } from './navLinks';
+import { MobileNavLinks } from './mobileNavLinks';
+
+
+// Using the styled-components, I have created divs and included the CSS/Styling for my NavbarContainer div, LeftSection div, and MiddleSection div and RightSection div in my jsx file.
+// I also separated the Navbar elements in their own folder in components. This file contains the main container that is then split up into three sections. The insides of these 3 
+// sections are created and styled in navLink.jsx inside of this folder as well.
+
+
+const NavbarContainer = styled.div`
+    width: 100%;
+    height: 60px;
+    box-shadow: 0 1px 3px rgba(15, 15, 15, 0.13);
+    display: flex;
+    align-items: center;
+    padding: 0 1.5em;
+`;
+
+const LeftSection = styled.div`
+    display: flex;
+
+`;
+
+const MiddleSection = styled.div`
+    display: flex;
+    flex: 2;
+    height: 100%;
+    justify-content: center;
+`;
+
+const RightSection = styled.div`
+    display: flex;
+`;
+
+export function NavBar(props) {
+
+    const isMobile = useMediaQuery({ maxWidth: DeviceSize.mobile })
+
+    return <NavbarContainer>
+        <LeftSection>
+            <Logo />
+        </LeftSection>
+        <MiddleSection>
+            {!isMobile && <NavLinks />}
+        </MiddleSection>
+        <RightSection>
+            {!isMobile && <Login />}
+            {isMobile && <MobileNavLinks />}
+        </RightSection>
+    </NavbarContainer>
+}
